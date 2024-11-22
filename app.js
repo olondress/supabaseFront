d1bf7b934cfb75b7f1b6a4356f52def07eddb7fb
Ricardo Junior <ricardojr.unifor@gmail.com>
2024-11-21 21:12:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const updateProductPrice = document.querySelector('#update-price');
 
 
 async function fetchProducts() {
-  const response = await fetch('http://localhost:3000/products');
+  const response = await fetch('http://54.165.9.64:3000/products');
   const products = await response.json();
 
   productList.innerHTML = '';
@@ -53,7 +53,7 @@ addProductForm.addEventListener('submit', async event => {
 
 
 async function addProduct(name, description, price) {
-  const response = await fetch('http://localhost:3000/products', {
+  const response = await fetch('http://54.165.9.64:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -64,7 +64,7 @@ async function addProduct(name, description, price) {
 }
 
 async function deleteProduct(id) {
-  const response = await fetch(`http://localhost:3000/products/${id}`, {
+  const response = await fetch(`http://54.165.9.64:3000/products/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
@@ -85,7 +85,7 @@ updateProductForm.addEventListener('submit', async event => {
 });
 
 async function updateProduct(id, name, description, price) {
-  const response = await fetch(`http://localhost:3000/products/${id}`, {
+  const response = await fetch(`http://54.165.9.64:3000/products/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
